0fdf59a97fda90f1925416e23080badc76fe2bff
veight <lightVeight@users.noreply.github.com>
2025-10-17 21:13:26 +0100

    rm directory check

--- src/cmd/rm.rs
+++ src/cmd/rm.rs
@@ -6,7 +6,14 @@ pub fn run(args: &[OsString]) -> Result<i32, anyhow::Error> {
     if args.len() != 1 {
         bail!("rm: expected 1 argument, got {}", args.len());
     }
+
     let path = Path::new(&args[0]);
+    let meta = std::fs::symlink_metadata(path)
+        .with_context(|| format!("rm: cannot access '{}'", path.display()))?;
+
+    if meta.file_type().is_dir() {
+        bail!("rm: cannot remove '{}': Is a directory", path.display());
+    }
 
     std::fs::remove_file(path)
         .with_context(|| format!("rm: cannot remove '{}'", path.display()))?;
